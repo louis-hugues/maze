20877ee067ad3bc3aea9b25441a3555a022a1bf7
louis-hugues <dum@xunuda.com>
2015-03-27 17:02:07 +0100

    wand

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -7,11 +7,49 @@ public class Maze
 
 {
 
+    class Wanderer {
+        int x,y;
+
+        void Wanderer(int x, int y ){
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    String maze[] = {
+            "######################################",
+            "#                                    #",
+            "#                                    #",
+            "#                                    #",
+            "######################################",
+
+    };
+
+
+    public void Maze(){
+
+    }
+
+
+
+
+    void print(){
+        for (String s : maze){
+            System.out.println(s);
+        }
+    }
+
+
 
 
     public static void  main(String args[]){
 
         System.out.println("main");
+        Maze m = new Maze();
+        m.print();
+
+        System.out.println("Done");
 
     }
 
