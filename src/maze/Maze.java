f48f5cc15eef07beb3e67bc7da5333eaaa2e8a20
louis-hugues <dum@xunuda.com>
2015-03-27 16:37:51 +0100

    Initial commit

---
+++ src/maze/Maze.java
@@ -0,0 +1,19 @@
+package maze;
+
+/**
+ * Created by huguesl on 27/03/15.
+ */
+public class Maze
+
+{
+
+
+
+    public static void  main(String args[]){
+
+        System.out.println("main");
+
+    }
+
+
+}
